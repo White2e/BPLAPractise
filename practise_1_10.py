0110853d1979f1e3773f1cbb42366558c05f577a
zubkoveu <zubkoveu@mail.ru>
2024-06-29 23:03:29 +0400

    practise 28 06 2024

--- practise_1_10.py
+++ practise_1_10.py
@@ -12,3 +12,40 @@ print(drones_data)
 altitudes = drones_data[:, 4]
 max_altitudes = np.max(altitudes)
 print(f"Максимальная высота полета: {max_altitudes}")
+
+long_flight_drones = drones_data[drones_data[:, 1] > 30]  # фильтруем строки массива на основании бинарного вектора, сформированного по условию
+print(long_flight_drones)
+
+total_dist = np.sum(drones_data[:, 2])
+print(f"Всего пройдено: {total_dist}")
+
+drone_idds = drones_data[:, 0]
+flight_times = drones_data[:, 1]
+altitudes = drones_data[:, 4]
+
+plt.figure(figsize=(10, 6))
+
+plt.subplot(1, 2, 1)
+plt.bar(drone_idds, flight_times, color='blue')
+plt.xlabel('ID БПЛА')
+plt.ylabel('Время полета (минуты)')
+plt.title('Время полета БПЛА')
+
+plt.subplot(1, 2, 2)
+plt.bar(drone_idds, altitudes, color='red')
+plt.xlabel('ID БПЛА')
+plt.ylabel('Высота полета (минуты)')
+plt.title('Высота полета БПЛА')
+
+plt.show()
+
+convertation_matrix = np.array([
+    [1, 0],
+    [0, 0.277778]
+])
+
+speed_kmh = drones_data[:, 3]
+speed_ms = speed_kmh * convertation_matrix[1, 1]
+
+print(f"Скорость - {speed_kmh} км/ч")
+print(f"Скорость - {speed_ms} м/с")
